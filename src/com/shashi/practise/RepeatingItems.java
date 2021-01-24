4eaa29fc136a2183f951a3301ba49638dbf703cc
shashi <0905shashi@gmail.com>
2021-01-24 22:26:52 +0530

    Renamed variable in the class

--- src/com/shashi/practise/RepeatingItems.java
+++ src/com/shashi/practise/RepeatingItems.java
@@ -10,18 +10,18 @@ public class RepeatingItems {
 	public static void main(String[] args) {
 		
 		Integer[] arr = {1,3,23,11,44,3,23,2,3,11}; 
-		Map<Integer, Integer> map = new HashMap<>();
+		Map<Integer, Integer> freqMap = new HashMap<>();
 		
 		for(Integer a : arr) {
-			if(map.containsKey(a)) {
-				map.put(a, map.get(a)+1);
+			if(freqMap.containsKey(a)) {
+				freqMap.put(a, freqMap.get(a)+1);
 			}
 			else {
-				map.put(a, 1);
+				freqMap.put(a, 1);
 			}
 		}
 		
-		List<Integer> keys = map.keySet().stream().filter(key->map.get(key) > 1).collect(Collectors.toList());
+		List<Integer> keys = freqMap.keySet().stream().filter(key->freqMap.get(key) > 1).collect(Collectors.toList());
 		keys.forEach(e->{System.out.println(e);});
 	}
 
